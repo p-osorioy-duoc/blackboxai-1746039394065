cdbd19ce3f2a5f246c9f53908717d9365e4105f9
p-osorioy-duoc <137123810+p-osorioy-duoc@users.noreply.github.com>
2025-04-30 15:20:09 -0400

    Add workshop/views.py 1746040808602

--- workshop/views.py
+++ workshop/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Vehicle, RepairRecord
-from .forms import VehicleForm, RepairRecordForm
+from .models import Vehicle, RepairRecord, Owner
+from .forms import VehicleForm, RepairRecordForm, OwnerForm
+from django.utils import timezone
+from django.db.models import Q
 
 def vehicle_list(request):
+    query = request.GET.get('q', '')
     vehicles = Vehicle.objects.all().order_by('-created_at')
-    return render(request, 'workshop/vehicle_list.html', {'vehicles': vehicles})
+    if query:
+        vehicles = vehicles.filter(
+            Q(license_plate__icontains=query) |
+            Q(brand__icontains=query) |
+            Q(model__icontains=query) |
+            Q(owner__name__icontains=query)
+        )
+    return render(request, 'workshop/vehicle_list.html', {'vehicles': vehicles, 'query': query})
 
 def vehicle_create(request):
     if request.method == 'POST':
@@ -30,3 +40,22 @@ def vehicle_detail(request, pk):
     vehicle = get_object_or_404(Vehicle, pk=pk)
     repairs = vehicle.repairs.all().order_by('-date')
     return render(request, 'workshop/vehicle_detail.html', {'vehicle': vehicle, 'repairs': repairs})
+
+def maintenance_calendar(request):
+    today = timezone.now().date()
+    future_maintenances = RepairRecord.objects.filter(next_maintenance_date__gte=today).order_by('next_maintenance_date')
+    return render(request, 'workshop/maintenance_calendar.html', {'future_maintenances': future_maintenances})
+
+def owner_list(request):
+    owners = Owner.objects.all().order_by('name')
+    return render(request, 'workshop/owner_list.html', {'owners': owners})
+
+def owner_create(request):
+    if request.method == 'POST':
+        form = OwnerForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('owner_list')
+    else:
+        form = OwnerForm()
+    return render(request, 'workshop/owner_form.html', {'form': form})
