a85802656813e635bdd8a583b80d68d676afec63
p-osorioy-duoc <137123810+p-osorioy-duoc@users.noreply.github.com>
2025-04-30 15:20:12 -0400

    Add workshop/urls.py 1746040812237

--- workshop/urls.py
+++ workshop/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('vehiculo/nuevo/', views.vehicle_create, name='vehicle_create'),
     path('reparacion/nueva/', views.repair_create, name='repair_create'),
     path('vehiculo/<int:pk>/', views.vehicle_detail, name='vehicle_detail'),
+    path('mantenciones/futuras/', views.maintenance_calendar, name='maintenance_calendar'),
+    path('propietarios/', views.owner_list, name='owner_list'),
+    path('propietarios/nuevo/', views.owner_create, name='owner_create'),
 ]
